0708d54b03889f7181ec462fb8a3a0d3329470db
Stone Comstock <comstockstone@yahoo.com>
2022-12-01 19:21:31 -0800

    Fetch all character data for home page with useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from './components/ui/Header';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className='container'>
       <Header />
